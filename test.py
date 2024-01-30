1b042f5762d25d501dc20ade70a4d0a3ee53b2db
abhi <imababhishek@gmail.com>
2024-01-30 14:58:43 +0530

    file updated

--- test.py
+++ test.py
@@ -10,3 +10,7 @@ with open(path,'w') as f:
 
 from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData
 
+custdataobj=CustomData(1.52,62.2,58.0,7.27,7.33,4.55,"premium","F","VS2")
+data=custdataobj.get_data_as_dataframe()
+
+print(data)
